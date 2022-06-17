48695e59ab4e21aa60ca42c70a124bb551354515
lakshaycodesgit <lakshay.gulati.ug20@nsut.ac.in>
2022-06-17 08:01:04 +0530

    new files added

--- Stacks/src/StackMain.java
+++ Stacks/src/StackMain.java
@@ -1,18 +1,23 @@
+import java.util.Arrays;
+
 public class StackMain {
     public static void main(String[] args) throws StackException {
-        CustomStack stack = new DynamicStack(5);
-        stack.push(50);
-        stack.push(51);
-        stack.push(52);
-        stack.push(53);
-        stack.push(54);
-        stack.push(55);
+        TwoStack stack = new TwoStack();
+        stack.push1(50);
+        stack.push1(51);
+        stack.push1(52);
+        stack.push2(53);
+        stack.push2(54);
+        stack.push2(55);
+        System.out.println(Arrays.toString(TwoStack.arr));
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        System.out.println(stack.pop1());
+        System.out.println(stack.pop2());
+        System.out.println(stack.pop1());
+        System.out.println(stack.pop2());
+        System.out.println(stack.pop1());
+        System.out.println(stack.pop2());
+
 
 
 
--- Stacks/src/TwoStack.java
+++ Stacks/src/TwoStack.java
@@ -1,2 +1,44 @@
 public class TwoStack {
+    private static final int size = 10;
+    private int start1 = -1;
+    private int start2 = size;
+    public static int[] arr;
+
+    public TwoStack(){
+        arr = new int[size];
+    }
+    public void push1 (int item) throws StackException{
+        if((start2-start1) > 1){
+            start1++;
+            arr[start1]=item;
+        }
+        else
+            throw new StackException("filled filled");
+    }
+    public void push2 (int item) throws StackException{
+        if((start2-start1) > 1){
+            start2--;
+            arr[start2]=item;
+        }
+        else
+            throw new StackException("filled filled");
+    }
+    public int pop1() throws StackException{
+        int ans = 0;
+        if(start1>=0){
+            ans = arr[start1--];
+        }
+        else
+            throw new StackException("empty");
+        return ans;
+    }
+    public int pop2() throws StackException{
+        int ans = 0;
+        if(start2<=size-1){
+            ans =  arr[start2++];
+        }
+        else
+            throw new StackException("empty");
+        return ans;
+    }
 }
